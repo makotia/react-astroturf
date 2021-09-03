13efa0127048975cd9bb43e3b2566b934de6d16b
makotia <3mdevmail@gmail.com>
2021-09-04 01:31:55 +0900

    init react app

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.tsx?$/,
+        options: {
+          configFile: 'tsconfig.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      src: path.resolve(__dirname, '/src'),
+    },
+  },
+  output: {
+    filename: 'static/js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}
