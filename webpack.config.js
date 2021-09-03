ba2a161aa28ec60c556fa127e99221d3b3ac8d71
makotia <3mdevmail@gmail.com>
2021-09-04 02:27:13 +0900

    astroturf の設定等を追加してみた (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const path = require("path")
 
-const htmlWebpackPlugin = require('html-webpack-plugin')
+const htmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        loader: 'ts-loader',
+        use: [
+          "babel-loader",
+          {
+            loader: "astroturf/loader",
+            options: { extension: ".module.css" },
+          },
+          "ts-loader",
+        ],
         test: /\.tsx?$/,
-        options: {
-          configFile: 'tsconfig.json',
-        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
     alias: {
-      src: path.resolve(__dirname, '/src'),
+      src: path.resolve(__dirname, "/src"),
     },
   },
   output: {
-    filename: 'static/js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "static/js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
 }
